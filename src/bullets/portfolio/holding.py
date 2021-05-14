7b24b9751a00c34c30b6de81003b1f4da31ac41f
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2021-05-14 14:49:06 -0400

    Add portfolio

---
+++ src/bullets/portfolio/holding.py
@@ -0,0 +1,23 @@
+class Holding:
+    def __init__(self, ticker: str):
+        """
+        Initializes the required variables for the Client
+
+        Args:
+            token (str): API key from FinancialModelPrep
+        """
+        self.ticker = ticker
+        self.nb_shares = 0
+        self.avg_price = 0
+
+    def add_shares(self, nb_shares: float, price: float):
+        """
+        Args:
+            nb_shares: Number of new shares
+            price: Price of the new shares
+
+        Raises:
+        ZeroDivisionError: No more shares will be left in the holding.
+        """
+        self.avg_price = (self.avg_price * self.nb_shares + price * nb_shares)/(self.nb_shares + nb_shares)
+        self.nb_shares = self.nb_shares + nb_shares
