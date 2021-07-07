b37f641291cda88aee6d339f4a8fc9dc3dd8e51b
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2021-07-06 23:16:14 -0400

    Fetch data using Asyncio

--- src/bullets/data_source/data_source_interface.py
+++ src/bullets/data_source/data_source_interface.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import aiohttp
+import asyncio
 
 __all__ = ["Resolution", "DataSourceInterface"]
 
@@ -15,5 +17,25 @@ class DataSourceInterface:
     def __init__(self):
         self.timestamp = None
 
-    def get_price(self, symbol: str):
+    def get_price(self, symbol: str, timestamp:datetime = None):
         pass
+
+    @staticmethod
+    def request(url: str) -> str:
+        """
+        Performs a request on the requested endpoint at the base url.
+        Args:
+            url (str): address with endpoint to retrieve data
+
+        Returns:
+            JSON string with the response content
+        """
+
+        async def fetch(url: str) -> str:
+            if url:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as response:
+                        if response.status == 200:
+                            return await response.text()
+
+        return asyncio.get_event_loop().run_until_complete(fetch(url))
