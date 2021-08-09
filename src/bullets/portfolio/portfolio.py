36931b3f5adbabd3a4540ad64ddab2297aefc41f
Philippe Geukers <philippe.geukers.1@ens.etsmtl.ca>
2021-08-08 20:07:40 -0400

    #27 Log orders with their type

--- src/bullets/portfolio/portfolio.py
+++ src/bullets/portfolio/portfolio.py
@@ -35,6 +35,9 @@ class Portfolio:
         self.pending_sell_stop_orders = []
 
     def market_order(self, symbol: str, nb_shares: float):
+        return self.__order(symbol, nb_shares, "Market Order")
+
+    def __order(self, symbol: str, nb_shares: float, order_type: str):
         """
         Order a stock at market price + slippage
         Args:
@@ -44,7 +47,7 @@ class Portfolio:
         """
         theoretical_price = self.data_source.get_price(symbol, self.timestamp)
         transaction = self.__validate_and_create_transaction__(symbol, nb_shares, theoretical_price,
-                                                               self.slippage_percent, self.transaction_fees)
+                                                               self.slippage_percent, self.transaction_fees, order_type)
         self.transactions.append(transaction)
         if transaction.status == Status.SUCCESSFUL:
             self.__put_holding__(symbol, nb_shares, transaction.simulated_price)
@@ -97,22 +100,22 @@ class Portfolio:
         for order in self.pending_buy_stop_orders:
             price = self.data_source.get_price(order.symbol)
             if price <= order.price:
-                self.market_order(order.symbol, order.nb_shares)
+                self.__order(order.symbol, order.nb_shares, "Buy Stop Order")
 
         for order in self.pending_sell_stop_orders:
             price = self.data_source.get_price(order.symbol)
             if price >= order.price:
-                self.market_order(order.symbol, order.nb_shares)
+                self.__order(order.symbol, order.nb_shares, "Sell Stop Order")
 
         for order in self.pending_buy_limit_orders:
             price = self.data_source.get_price(order.symbol)
             if price <= order.price:
-                self.market_order(order.symbol, order.nb_shares)
+                self.__order(order.symbol, order.nb_shares, "Buy Limit Order")
 
         for order in self.pending_sell_limit_orders:
             price = self.data_source.get_price(order.symbol)
             if price >= order.price:
-                self.market_order(order.symbol, order.nb_shares)
+                self.__order(order.symbol, order.nb_shares, "Sell Limit Order")
 
     def update_and_get_balance(self):
         """
@@ -129,7 +132,7 @@ class Portfolio:
         return round(self.update_and_get_balance() / self.start_balance * 100 - 100, 2)
 
     def __validate_and_create_transaction__(self, symbol: str, nb_shares: float, theoretical_price: float,
-                                            slippage_percent: int, transaction_fees: int):
+                                            slippage_percent: int, transaction_fees: int, order_type: str):
         """
         Validates and creates a transaction
         Args:
@@ -151,7 +154,7 @@ class Portfolio:
             else:
                 status = Status.FAILED_INSUFFICIENT_FUNDS
         return Transaction(symbol, nb_shares, theoretical_price, simulated_price, self.timestamp, self.cash_balance,
-                           status, transaction_fees)
+                           status, transaction_fees, order_type)
 
     def __get_slippage_price__(self, theoretical_price: float, slippage_percent: int, symbol: str) -> float:
         # todo: see https://github.com/AlgoETS/BullETS/issues/46
