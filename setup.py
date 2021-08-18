e5ab08982e7f83a78ea265d06e308a20141435ca
Tristan Fecteau <tristan.fecteau.1@ens.etsmtl.ca>
2021-08-18 17:56:25 -0400

    removed requirements, install_requires and changed version number

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-requirements = []
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 readme = ''
 with open('README.md') as f:
     readme = f.read()
@@ -14,13 +10,12 @@ setup(
     description='BullETS is a Python package designed to help with the development of algorithmic trading strategies.',
     long_description=readme,
     long_description_content_type='text/markdown',
-    version='0.0.1a',
+    version='0.0.2',
     license='Apache 2.0',
     python_requires='>=3.7',
     url='https://github.com/AlgoETS/BullETS',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    install_requires=requirements,
     package_data={'': ['requirements.txt']},
     include_package_data=True
 )
