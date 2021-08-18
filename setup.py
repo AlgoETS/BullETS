96340e62dfa8a38a8320b99cdb767769c96970a0
Tristan Fecteau <tristan.fecteau.1@ens.etsmtl.ca>
2021-08-18 18:32:05 -0400

    changed version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description='BullETS is a Python package designed to help with the development of algorithmic trading strategies.',
     long_description=readme,
     long_description_content_type='text/markdown',
-    version='0.0.3',
+    version='0.0.4',
     license='Apache 2.0',
     python_requires='>=3.7',
     url='https://github.com/AlgoETS/BullETS',
