67a4b44e008249edd6c3126c9e39a88779f48eb7
Marco Caballero <caballerom@hotmail.ca>
2021-09-13 20:00:26 -0400

    Initial Commit

---
+++ src/bullets/data_indicators/indicators.py
@@ -0,0 +1,15 @@
+
+class Indicators:
+    """
+    Defines general indicators
+    """
+    def sma(self, days: int):
+        """
+        Calculates the Simple Moving Average
+        Args:
+            days:
+
+        Returns:
+
+        """
+        return days
