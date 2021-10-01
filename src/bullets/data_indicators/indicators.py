1c264110374c789ea3d31138d815c6d69c7e5234
sam3space <sam_lafrance@outlook.com>
2021-10-01 17:02:24 -0400

    Ajout du MACD et EMA

--- src/bullets/data_indicators/indicators.py
+++ src/bullets/data_indicators/indicators.py
@@ -2,37 +2,112 @@ from bullets.data_source.data_source_interface import DataSourceInterface, Resol
 from bullets.runner import Runner
 from datetime import datetime, timedelta
 
-class Indicators:
 
-    @staticmethod
-    def simple_moving_avg(data_source: DataSourceInterface, symbol: str, target_date: datetime, days: int):
+class Indicators:
+    def __init__(self, data_source: DataSourceInterface):
+        self.data_source = data_source
+
+    def sma(self, symbol: str, period: int, date: datetime = None):
         """
         Calculates the Simple Moving Average
         Args:
-            data_source: current data source
             symbol: Stock symbol
-            target_date: Date of average / start date
-            days: number of days for the average
+            period: number of days for the average
+            date: Date of average / start date
         Returns:
             sma: Average stock price for the given range
         """
-        ##TODO try with getting first
-        data_source.get_price(symbol=symbol, timestamp=(target_date - timedelta(days=days)))
+
+        if date is None:
+            date = self.data_source.timestamp
+        else:
+            date = date
 
         values = []
 
-        for x in range(days):
+        for x in range(period):
             #Make sure market is open
-            while not Runner._is_market_open(target_date, Resolution.DAILY):
-                target_date - timedelta(days=1)
-
-            #Fetch stock value
-            values.append(data_source.get_price(symbol=symbol, timestamp=target_date))
+            while not Runner._is_market_open(date, Resolution.DAILY):
+                date -= timedelta(days=1)
 
             ##Go back one day
-            target_date - timedelta(days=1)
+            date -= timedelta(days=1)
+
+        for x in range(period):
+            # Make sure market is open
+            while not Runner._is_market_open(date, Resolution.DAILY):
+                date += timedelta(days=1)
+
+            #Fetch stock value
+            price = self.data_source.get_price(symbol=symbol, timestamp=date)
+            if price is not None:
+                values.append(price)
+
+            ##Go forward one day
+            date += timedelta(days=1)
 
         #Calculate SMA
         sma = sum(values) / len(values)
 
         return sma
+
+    def ema(self, symbol: str, period: int, date: datetime = None, smoothing: int = 2):
+        """
+        Calculates the Simple Moving Average
+        Args:
+            symbol: Stock symbol
+            period: number of days for the average
+            date: Date of average / start date
+            smoothing: weighted importance of latest data, higher number gives more weight to more recent data
+        Returns:
+            ema: Exponential average stock price for the given range
+        """
+
+        if date is None:
+            date = self.data_source.timestamp
+        else:
+            date = date
+
+        multiplier = smoothing/(period + 1)
+
+        for x in range(period):
+            #Make sure market is open
+            while not Runner._is_market_open(date, Resolution.DAILY):
+                date -= timedelta(days=1)
+
+            ##Go back one day
+            date -= timedelta(days=1)
+
+        ema = self.sma(symbol, period, date)
+        date += timedelta(days=1)
+
+        for x in range(period):
+            # Make sure market is open
+            while not Runner._is_market_open(date, Resolution.DAILY):
+                date += timedelta(days=1)
+
+            price = self.data_source.get_price(symbol=symbol, timestamp=date)
+            if price is not None:
+                ema = price*multiplier + ema*(1-multiplier)
+
+            ##Go forward one day
+            date += timedelta(days=1)
+
+        return ema
+
+    def macd(self, symbol, date: datetime = None):
+        """
+        Calculates the Simple Moving Average
+        Args:
+            symbol: Stock symbol
+            date: Calculated date / start date
+        Returns:
+            MACD
+        """
+
+        if date is None:
+            date = self.data_source.timestamp
+        else:
+            date = date
+
+        return self.ema(symbol, 12, date) - self.ema(symbol, 26, date)
--- src/test/test_indicators.py
+++ src/test/test_indicators.py
@@ -8,9 +8,10 @@ from bullets.data_source.data_source_interface import DataSourceInterface
 
 class TestIndicators(unittest.TestCase):
 
-    @mock.patch('bullets.data_source.data_source_interface.DataSourceInterface.get_price', return_value=1)
+    # @mock.patch('bullets.data_source.data_source_interface.DataSourceInterface.get_price', return_value=1)
     def test_sma(self, mock_get_price):
-        sma = Indicators.simple_moving_avg(data_source=DataSourceInterface(), symbol="TEST", target_date=datetime(2021, 1, 1), days=5)
+        indicateur = Indicators(DataSourceInterface())
+        sma = Indicators.sma(symbol="TEST", target_date=datetime(2021, 1, 1), days=5)
         self.assertEqual(1, sma)
 
 
