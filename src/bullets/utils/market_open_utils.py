35689f7a4c92e4ec912752ddc1c0bea0215688bc
Raphael-Leb <62116334+Raphael-Leb@users.noreply.github.com>
2022-01-30 13:23:42 -0500

    Second revision of #84
    Added documentation for is_market_open()

--- src/bullets/utils/market_open_utils.py
+++ src/bullets/utils/market_open_utils.py
@@ -3,6 +3,13 @@ from bullets.data_source.data_source_interface import Resolution
 from bullets.utils.holiday_date_util import us_holiday_list
 
 def is_market_open(date: datetime, resolution: Resolution=Resolution.DAILY) -> bool:
+    """
+    Returns if the market is open on a given day
+    Args:
+        date: Checks if the market is open on the date given
+        optional: resolution: Will check if market is open at a specific time if the resolution demands it
+    Returns: True if the market is open, else False
+    """
         if date.weekday() >= 5:
             return False
 
