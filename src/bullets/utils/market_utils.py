5c1fc40ec1a9743b22b932fbe9d8cc004f2dfec8
Tristan Fecteau <tristan.fecteau.1@ens.etsmtl.ca>
2022-01-30 13:58:51 -0500

    changed file name & reformatted file to fit bullets standards

--- src/bullets/utils/market_utils.py
+++ src/bullets/utils/market_utils.py
@@ -2,12 +2,13 @@ from datetime import datetime, timedelta, date
 from bullets.data_source.data_source_interface import Resolution
 from bullets.utils.holiday_date_util import us_holiday_list
 
-def is_market_open(date: datetime, resolution: Resolution=Resolution.DAILY) -> bool:
+
+def is_market_open(date: datetime, resolution: Resolution = Resolution.DAILY) -> bool:
     """
     Returns if the market is open on a given day
     Args:
         date: Checks if the market is open on the date given
-        optional: resolution: Will check if market is open at a specific time if the resolution demands it
+        resolution: Will check if market is open at a specific time if the resolution demands it
     Returns: True if the market is open, else False
     """
     if date.weekday() >= 5:
@@ -23,6 +24,7 @@ def is_market_open(date: datetime, resolution: Resolution=Resolution.DAILY) -> b
 
         return date not in us_holiday_list(date.year)
 
+
 def get_date_in_x_market_days_away(lapse: int) -> date:
     """
     Gets the date that is x amount of market day(s) away
@@ -34,6 +36,6 @@ def get_date_in_x_market_days_away(lapse: int) -> date:
     today = datetime.today()
     while countdown > 0:
         today = today + timedelta(days=+1)
-        if (is_market_open(today)): 
+        if is_market_open(today):
             countdown -= 1
     return today
