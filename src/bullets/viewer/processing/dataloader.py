e349c22e275f8707492f268886d933a96486624e
maxew42 <maximiliendufau@gmail.com>
2022-03-25 17:08:16 -0400

    + fixs + Total fees + Code refactor + New pages : Graph Summary, Compare, Tutorial

--- src/bullets/viewer/processing/dataloader.py
+++ src/bullets/viewer/processing/dataloader.py
@@ -25,4 +25,5 @@ def load_strategy(path_to_log):
         data['failed_transactions'] = (df_transac.shape[0]-len(df_transac[df_transac.status == "SUCCESSFUL"]))/df_transac.shape[0]  #TODO : make it derive directly fromthe Order classin portofolio
         data['num_transactions'] = len(df_transac)
         data['volume_transactions'] = df_successful_transac['total_price'].sum()
+        data['total_fees'] = df_successful_transac['transaction_fees'].sum()
     return data, df_transac
--- src/bullets/viewer/viewer_app.py
+++ src/bullets/viewer/viewer_app.py
@@ -16,44 +16,77 @@ try:
 except SystemExit as e:
     os._exit(e.code)
 
-print(args)
 
-if 'loaded' not in st.session_state.keys() or st.session_state["loaded"]==False:
-    print("not loaded")
+def Home():
     file_dict = list_log_files(args.path_to_log)
-    st.session_state['file_dict'] = file_dict
-    st.session_state['loaded'] = False
-st.markdown("# BullETS Strategy Viewer")
-
-selected_file = st.selectbox("Select a log file", file_dict.keys())
-is_selected = st.button("Load log file")
-if args.file != "":
-    selected_file=args.file
-    is_selected = True
-    args.file = ""
 
 
-if is_selected:
-    print("is_selected")
-    st.markdown("# Analysing {}".format(selected_file))
-    st.markdown("---")
-    if not st.session_state['loaded']:
-        data, df_transac = load_strategy(file_dict[selected_file])
-        st.session_state['loaded'] = True
-    else:
+    if 'loaded' not in st.session_state.keys() or st.session_state["loaded"]==False:
+        print("not loaded")
         st.session_state['file_dict'] = file_dict
+        st.session_state['loaded'] = False
+    st.markdown("# BullETS Strategy Viewer")
 
-    st.sidebar.markdown("Nice sidebar")
+    selected_file = st.selectbox("Select a log file", file_dict.keys())
+    is_selected = st.button("Load log file")
+    if args.file != "":
+        selected_file=args.file
+        is_selected = True
+        args.file = ""
 
-    col_profit, col_final_balance, col_final_cash = st.columns(3) #TODO : fetch proper currency
-    col_profit.metric("Profit", "{:.2%}".format(data['profit']/100),"{}".format("STONKS" if data['profit'] >0 else "NOT STONKS"))
-    col_final_balance.metric("Final balance", "{:.2f}".format(data['final_balance']),"Starting : {:.2f}".format(data['starting_balance']), delta_color="off")
-    col_final_cash.metric("Final Cash", "{:.2f}".format(data['final_cash']))
+    currency = "$" #TODO: to store and to load from the json file
 
-    col_num_transactions, col_failed_transactions, col3 = st.columns(3)
-    col_num_transactions.metric("Volume Transactions", "{:.2f}".format(data['volume_transactions']),"Successful Transactions: {}".format(data['num_transactions']), delta_color="off")
-    col_failed_transactions.metric("Failed Transactions", "{:.2%}".format(data['failed_transactions']))
+    if is_selected:
+        st.markdown("# Analysing {}".format(selected_file))
+        st.markdown("---")
+        if not st.session_state['loaded']:
+            data, df_transac = load_strategy(file_dict[selected_file])
+            st.session_state['data'] = data
+            st.session_state['df_transac'] = df_transac
+            st.session_state['loaded'] = True
+            print(data)
+        else:
+            file_dict = list_log_files(args.path_to_log)
+            st.session_state['file_dict'] = file_dict
+            data, df_transac = st.session_state['data'], st.session_state['df_transac']
 
-    st.dataframe(df_transac)
-    st.plotly_chart(cash_balance_chart(df_transac))
-    #st.plotly_chart(candle_chart(data))
+        col_profit, col_final_balance, col_final_cash = st.columns(3)
+        col_profit.metric("Profit", "{:.2%}".format(data['profit']/100),"{}".format("STONKS" if data['profit'] >0 else "NOT STONKS"), delta_color="off")
+        col_final_balance.metric("Final balance", "{:.1f} {currency}".format(data['final_balance'], currency=currency),"{:.2f} {currency}".format(data['final_balance'] - data['starting_balance'], currency=currency))
+        col_final_cash.metric("Final Cash", "{:.1f} {currency}".format(data['final_cash'],currency = currency))
+
+        col_num_transactions, col_failed_transactions, col_total_fees = st.columns(3)
+        col_num_transactions.metric("Volume Transactions", "{:.1f} {currency}".format(data['volume_transactions'],currency = currency),"Successful Transactions: {}".format(data['num_transactions']), delta_color="off")
+        col_failed_transactions.metric("Failed Transactions", "{:.2%}".format(data['failed_transactions']))
+        col_total_fees.metric("Total fees", "{:.1f} {currency}".format(data['total_fees'], currency=currency))
+        st.markdown("---")
+
+        st.markdown("## Transactions DataFrame")
+        st.dataframe(df_transac)
+
+
+def SummaryGraph():
+
+    if not st.session_state['loaded']:
+        st.markdown("# Please start by loading a log file in the **HOME** section")
+    else:
+        file_dict = list_log_files(args.path_to_log)
+        st.session_state['file_dict'] = file_dict
+        data, df_transac = st.session_state['data'], st.session_state['df_transac']
+
+        st.markdown("# Graphical Summary")
+        st.markdown("---")
+
+        #TODO: Insert the actual graph summary
+        st.plotly_chart(cash_balance_chart(df_transac))
+
+def Compare():
+    st.markdown("Not yet implemented")
+
+def Tutorial():
+    st.markdown("*For a guide on how to install BullETS please refer to our [GitHub](https://github.com/AlgoETS/BullETS)*")
+
+pages = {'Home': Home, 'Summary Graph': SummaryGraph,'Compare': Compare, 'Tutorial': Tutorial}  # A dict with each pages,you can create a new page by creating a new function and adding it to this dict
+page = st.sidebar.selectbox('Navigation', pages.keys())
+
+pages[page]()
